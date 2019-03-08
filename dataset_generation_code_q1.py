cbfe0c37c3f314a5b6196ee39b72551913ed5dd1
PMSIITMANDI <raj258p@gmail.com>
2019-03-08 23:26:17 +0530

    Data genration code added

---
+++ dataset_generation_code_q1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+img_row=28
+img_col=28
+blue =(255,0,0)
+red=(0,0,255)
+thin = 1
+thick =3
+short =7
+long =15
+width = thin
+color =blue
+
+tx=14
+ty=18
+
+
+#############################################
+testimage = np.zeros((img_row,img_col,3), np.uint8)
+d=0
+for i in range(6,7):
+	for j in range(6,8):
+		for theta in range(0,180,15):
+			testimage = cv2.line(testimage,(j,i),(j+7,i),color,width)
+			rotationMatrix = cv2.getRotationMatrix2D((i,j),theta,1)
+			translationMatrix = np.float32([[1, 0, 100], [0, 1, 50]])
+			testimage = cv2.warpAffine(testimage,rotationMatrix,(img_row,img_col))
+			cv2.imwrite("./testimage/face-%d.jpg"%d,testimage)
+			d=d+1
+			testimage = np.zeros((img_row,img_col,3), np.uint8)
